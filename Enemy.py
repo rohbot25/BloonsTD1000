ff6d47e128c37e8b600c295a23a4283abd851896
Sam <sdwill@uvm.edu>
2024-10-07 14:53:01 -0400

    Added vars and functions

--- Enemy.py
+++ Enemy.py
@@ -1,8 +1,20 @@
 # The Enemy Class (aka, Fish Class)
 
 class Enemy:
+    hp = 5
+    points = 5
+    spd = 1.0
+    type = "fish"
+    position = [0, 0]
+
     def __init__(self, hp, points, spd, type):
         self.hp = hp
         self.points = points
         self.spd = spd
         self.type = type
+
+    def checkLocation(self):
+        return self.position
+
+    def updatePosition(self):
+        pass
