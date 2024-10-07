c18bda6c618fd4f9fd7e2cef9cdc87cbb1765245
Michael Scott Hayes <98482126+Mikey1336@users.noreply.github.com>
2024-10-07 15:00:55 -0400

    Add files via upload

---
+++ tower.py
@@ -0,0 +1,37 @@
+class Tower:
+    #Unique ID
+    ID = 0
+    #Attack Radius
+    radius = 2.0
+    
+    #Damage per attack
+    atk = 50
+
+    #Rate of attack
+    rate = 1;
+
+    #Name of tower
+    name = "Fisherman"
+
+    #Upgrade number, could change to a tuple if we want to add upgrade paths
+    #Should not be altered upon initializing
+    upg = 0
+    
+    
+    def __init__(self, ID, name, radius, atk, rate):
+        self.ID = ID
+        self.name = name
+        self.radius = radius
+        self.atk = atk
+        self.rate = rate
+
+    def upgrade():
+        upg += 1
+        radius += .5
+        atk += 25
+        rate += .5
+        
+
+    
+
+    
