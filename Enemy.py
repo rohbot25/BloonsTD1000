9f8ca634e56c298eafd42892589368283dac4184
Sam <sdwill@uvm.edu>
2024-10-07 14:37:59 -0400

    Base Enemy Class

--- Enemy.py
+++ Enemy.py
@@ -1,1 +1,8 @@
 # The Enemy Class (aka, Fish Class)
+
+class Enemy:
+    def __init__(self, hp, points, spd, type):
+        self.hp = hp
+        self.points = points
+        self.spd = spd
+        self.type = type
