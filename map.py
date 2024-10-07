4b8d997fc63b5e13a5cbb46c0f95cf43f39e3146
rohbot25 <ewrohan@gmail.com>
2024-10-07 14:35:22 -0400

    map class

---
+++ map.py
@@ -0,0 +1,17 @@
+#Map Class
+
+class MAP:
+    #list of coordinate points for path and hazards
+    def __init__(self,path,hazards):
+        self.size = 64
+        self.path = path
+        self.hazards = hazards
+
+    def get_path(self):
+        return self.path
+    
+    def get_hazards(self):
+        return self.hazards
+    
+    def get_size(self):
+        return self.size
