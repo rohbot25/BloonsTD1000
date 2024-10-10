2a154754ad7caf50ffb2f2f3c56070c5b931be1b
rohbot25 <ewrohan@gmail.com>
2024-10-10 00:33:26 -0400

    adding towers

--- Game.py
+++ Game.py
@@ -9,15 +9,16 @@ SCREEN_TITLE = "Fish Tower Defense"
 
 SPRITE_SCALING = 1.0 
 BALLOON_SPEED = 2.0
+BULLET_SPEED = 10.0
 
 class Balloon(arcade.Sprite):
     #BALOOOOONS
 
-    def __init__(self, image, scale, position_list):
+    def __init__(self, image, scale, path):
         #something about the image for the sprite
         super().__init__(image, scale)
         #path
-        self.position_list = position_list
+        self.path = path
         self.cur_position = 0
         #speed TODO change based on balloon type? 
         self.speed = BALLOON_SPEED
@@ -30,8 +31,8 @@ class Balloon(arcade.Sprite):
         start_y = self.center_y
 
         #end
-        dest_x = self.position_list[self.cur_position][0]
-        dest_y = self.position_list[self.cur_position][1]
+        dest_x = self.path[self.cur_position][0]
+        dest_y = self.path[self.cur_position][1]
 
         # difference
         x_diff = dest_x - start_x
@@ -62,8 +63,18 @@ class Balloon(arcade.Sprite):
             self.cur_position += 1
 
             # Reached the end of the list, start over.
-            #if self.cur_position >= len(self.position_list):
-                #self.cur_position = 0
+            if self.cur_position >= len(self.path):
+                self.cur_position = 0
+
+class Tower(arcade.Sprite):
+    def __init__(self, image, scale):
+        #something about the image for the sprite
+        super().__init__(image, scale)
+
+    def update(self,balloon):
+       pass 
+
+
 
 class Game(arcade.Window):
     """ Main application class. """
@@ -72,19 +83,19 @@ class Game(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         arcade.set_background_color(arcade.color.AMAZON)
-        basic_path_cords = [[0,3],[1,3],[2,3],[3,3],[4,3],[5,3],[6,3],[7,3],[8,3],[9,3]]
-        #second [] is for hazards
-        self.current_map = MAP(basic_path_cords,[])
         
-        #grid option
-        self.path_list = None
-        self.hazards_list = None
-        self.free_list = None
-        
-        #map option
+        #map
         self.texture = None
 
+        #balloons TODO will need to switch this to waves?
         self.balloons = None
+
+        #TODO will need to come from map to be able to be placed
+        self.towers = None
+        self.harpoons = None
+
+        self.frame_count = 0
+
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
@@ -92,6 +103,15 @@ class Game(arcade.Window):
         self.texture = arcade.load_texture("images/map.png")
             
         self.balloons = arcade.SpriteList()
+        self.towers = arcade.SpriteList()
+        self.harpoons = arcade.SpriteList()
+
+        # Add a tower
+        tower = arcade.Sprite("images/sungod.png", 0.5)
+        tower.center_x = 600
+        tower.center_y = 200
+        tower.angle = 180
+        self.towers.append(tower)
 
         position_list = [
             [0,220],
@@ -109,7 +129,7 @@ class Game(arcade.Window):
             [300,0]
         ]
 
-        balloon = Balloon("images/balloon.png",0.5,position_list)
+        balloon = Balloon("images/balloon.png",0.25,position_list)
 
         balloon.center_x = position_list[0][0]
         balloon.center_y = position_list[0][1]
@@ -141,6 +161,8 @@ class Game(arcade.Window):
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 3, SCREEN_HEIGHT // 3, 750,375,self.texture)
 
         self.balloons.draw()
+        self.towers.draw()
+        self.harpoons.draw()
 
         # Draw grid overlay
         self.draw_grid()
@@ -161,6 +183,56 @@ class Game(arcade.Window):
     #update the position of the sprites
     def on_update(self,delta_time):
         self.balloons.update()
+
+        self.frame_count += 1
+
+        for tower in self.towers:
+            # First, calculate the angle to the player. We could do this
+            # only when the bullet fires, but in this case we will rotate
+            # the enemy to face the player each frame, so we'll do this
+            # each frame.
+
+            # Position the start at the enemy's current location
+            start_x = tower.center_x
+            start_y = tower.center_y
+
+            # Get the destination location for the bullet
+            dest_x = self.balloons[0].center_x
+            dest_y = self.balloons[0].center_y
+
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Set the enemy to face the player.
+            tower.angle = math.degrees(angle) - 90
+
+            # Shoot every 60 frames change of shooting each frame
+            if self.frame_count % 30 == 0:
+                bullet = arcade.Sprite("images/sun.png",.1)
+                bullet.center_x = start_x
+                bullet.center_y = start_y
+
+                # Angle the bullet sprite
+                bullet.angle = math.degrees(angle)
+
+                # Taking into account the angle, calculate our change_x
+                # and change_y. Velocity is how fast the bullet travels.
+                bullet.change_x = math.cos(angle) * BULLET_SPEED
+                bullet.change_y = math.sin(angle) * BULLET_SPEED
+
+                self.harpoons.append(bullet)
+
+        # Get rid of the bullet when it flies off-screen
+        for bullet in self.harpoons:
+            if bullet.top < 0:
+                bullet.remove_from_sprite_lists()
+
+        self.harpoons.update()
+
         
 def main():
     """ Main function """
